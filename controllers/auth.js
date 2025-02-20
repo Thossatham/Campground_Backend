bf60769642c0fc82bbc2bbd7aea823a78425bde7
Thossatham <thossatham1@gmail.com>
2025-02-20 10:48:20 +0700

    add cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,8 +13,11 @@ exports.register = async (req,res,next) => {
             password,
             role
         });
-        const token = user.getSignedJwtToken();
-        res.status(200).json({success:true,token});
+        // const token = user.getSignedJwtToken();
+        // res.status(200).json({success:true,token});
+
+        sendTokenResponse(user,200,res);
+
     }catch(err) {
         res.status(400).json({success:false});
         console.log(err.stack);
@@ -43,7 +46,28 @@ exports.login = async (req,res,next) => {
         return res.status(400).json({success:false, msg:'Invalid credentials'});
     }
 
+    // const token = user.getSignedJwtToken();
+
+    // res.status(200).json({success:true, token});
+    sendTokenResponse(user,200,res);
+};
+
+
+const sendTokenResponse = (user, statusCode, res) => {
+
     const token = user.getSignedJwtToken();
 
-    res.status(200).json({success:true, token});
-};
+    const options = {
+        expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
+        httpOnly: true
+    };
+
+    if(process.env.NODE_ENV==='production') {
+        options.secure=true;
+    }
+    
+    res.status(statusCode).cookie('token',token,options).json({
+        success: true,
+        token
+    });
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
@@ -11,7 +12,7 @@ const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
 app.use('/api/v1/hospitals', hospitals);
 app.use('/api/v1/auth', auth);
-
+app.use(cookieParser());
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
