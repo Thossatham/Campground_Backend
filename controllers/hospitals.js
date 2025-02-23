fe3f0c71bc9f0f77f0643f68c8013f5c4694ad3d
Thossatham <thossatham1@gmail.com>
2025-02-24 01:13:06 +0700

    add pagination

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -10,7 +10,7 @@ exports.getHospitals = async (req,res,next) => {
     const reqQuery = {...req.query};
 
     //Fields to exclude
-    const removeFields = ['select', 'sort'];
+    const removeFields = ['select', 'sort', 'page', 'limit'];
 
     //Loop over remove fields and delete them from reqQuery
     removeFields.forEach(param=>delete reqQuery[param]);
@@ -36,10 +36,33 @@ exports.getHospitals = async (req,res,next) => {
         query = query.sort('-createAt');
     }
 
+    //Pagination
+    const page = parseInt(req.query.page,10)||1;
+    const limit = parseInt(req.query.limit,10)||25;
+    const startIndex = (page-1)*limit;
+    const endIndex = page*limit;
     try{
+        const total = await Hospital.countDocuments();
+        query = query.skip(startIndex).limit(limit);
+        //Execute query
         const hospitals = await query;
-        console.log(req.query);
 
+        //Pagination result
+        const pagination = {};
+
+        if(endIndex<total) {
+            pagination.next = {
+                page:page+1,
+                limit
+            }
+        }
+
+        if(startIndex>0) {
+            pagination.prev = {
+                page:page-1,
+                limit
+            }
+        }
         res.status(200).json({success:true, count: hospitals.length, data:hospitals});
     } catch(err) {
         res.status(400).json({success:false});
