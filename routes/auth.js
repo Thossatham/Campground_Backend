146dd3ba1f99d825a5301aa87f7ac4fdc43a05e6
Thossatham <thossatham1@gmail.com>
2025-03-03 10:42:01 +0700

    add delete user function

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const {register, login, getMe, logout} = require('../controllers/auth');
+const {register, login, getMe, logout, deleteUser} = require('../controllers/auth');
 
 const router = express.Router();
 
 const {protect} = require('../middleware/auth');
 
 router.post('/register', register);
-router.post('/login',login);
-router.post('/logout',protect,logout);
-router.get('/me',protect,getMe);
+router.post('/login', login);
+router.post('/logout', protect, logout);
+router.get('/me', protect, getMe);
+router.delete('/delete', protect, deleteUser); // Updated the function name
 
 module.exports = router;
