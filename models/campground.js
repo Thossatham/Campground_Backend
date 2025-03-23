8205b703ea63514f860294e04553a7f74faa3fc0
Thossatham <thossatham1@gmail.com>
2025-03-23 14:48:40 +0700

    add picture and dailyrate

--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,7 @@ const CampgroundSchema = new mongoose.Schema({
         required: [true, 'Please add a name'],
         unique: true,
         trim: true,
-        maxlength:[50,'Name can not be more than 50 characters']
+        maxlength: [50, 'Name cannot be more than 50 characters']
     },
     address: {
         type: String,
@@ -22,12 +22,20 @@ const CampgroundSchema = new mongoose.Schema({
     },
     postalcode: {
         type: String,
-        required: [true, 'Please add a postalcode'],
-        maxlength: [5, 'Postal Code can not be more than 5 digits']
+        required: [true, 'Please add a postal code'],
+        maxlength: [5, 'Postal Code cannot be more than 5 digits']
     },
     tel: {
         type: String,
-        required: [true, 'Please add a tel']
+        required: [true, 'Please add a telephone number']
+    },
+    picture: {
+        type: String,
+        required: [true, 'Please add a picture URL']
+    },
+    dailyrate: {
+        type: Number,
+        required: [true, 'Please add a daily rate']
     }
 }, {
     toJSON: { virtuals: true },
