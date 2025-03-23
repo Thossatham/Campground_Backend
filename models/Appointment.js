2f2544b75884f9c5784af72cd1abac7033bbc6a1
Thossatham <thossatham1@gmail.com>
2025-03-23 21:35:01 +0700

    adjust Appointment model

--- models/Appointment.js
+++ models/Appointment.js
@@ -3,47 +3,22 @@ const campground = require('./campground');
 
 const AppointmentSchema = new mongoose.Schema({
     apptDate: {
-        type: Date,
-        required:true
+        type: String,
+        required: true
     },
-    // checkInDate: {
-    //     type: Date,
-    //     required: true
-    // },
-    // checkOutDate: {
-    //     type: Date,
-    //     required: true,
-    //     validate: {
-    //         validator: function (val) {
-    //             return val > this.checkInDate; // Ensure check-out is after check-in
-    //         },
-    //         message: "Check-out date must be after check-in date"
-    //     }
-    // },
     user: {
-        type:mongoose.Schema.ObjectId,
+        type: String,
         ref: 'User',
         required: true
     },
     campground: {
-        type: mongoose.Schema.ObjectId,
+        type: String,
         ref: 'Campground',
         required: true
     },
     refcreateAt: {
-        type: Date,
-        default: Date.now
+        type: String
     }
 });
-
-// // Middleware to enforce max 3-night stay before saving
-// AppointmentSchema.pre('save', function (next) {
-//     const maxNights = 3;
-//     const nights = (this.checkOutDate - this.checkInDate) / (1000 * 60 * 60 * 24); // Convert ms to days
-//     if (nights > maxNights) {
-//         return next(new Error(`You can only book a maximum of ${maxNights} nights.`));
-//     }
-//     next();
-// });
 
 module.exports = mongoose.model('Appointment',AppointmentSchema);
